434b5d150ee81d0c7828b55cfb2f51cfb4d823f3
n-devs <n.devs260340@gmail.com>
2022-06-28 21:55:34 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  // Question 1
+  num = 0;
+  values = true;
+
+
+  onKey(event: any) {
+    this.num = event.target.value
+  }
+
+  ChangingValue(event: any) {
+
+    switch (event.target.value) {
+      case "0":
+        this.values = Number.isInteger(this.num)
+        break;
+      case "1":
+        this.values = !Number.isInteger(this.num)
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  // end Question 1
+
+
+
+
 }
